0c154d7a532b886159380baf8ee3797eacf8218a
Jon Stenqvist <jon.stenqvist@gmail.com>
2009-01-21 22:32:56 +0100

    Built the recorder class object

--- lib/fire_grabber/recorder.rb
+++ lib/fire_grabber/recorder.rb
@@ -1,12 +1,119 @@
+require 'rubygems'
+require 'activesupport'
+require 'ostruct'
+
 module FireGrabber
   class Recorder
     class_inheritable_hash :configuration
     self.configuration = {}
-    self.configuration[:dvgrab_exeutable] = 'dvgrab'
-    self.configuration[:output_file] = ''
     
-    def start!
-      raise "No output file specified"
+    def self.configure
+      yield config = OpenStruct.new(configuration)
+      configuration.merge!(config.marshal_dump)
     end
+    
+    # Default settings
+    configure do |config|
+      config.dvgrab_exeutable = 'dvgrab'
+      config.dvgrab_args = '-showstatus -noavc -f dv1'
+      config.output_file = ''
+      config.line_ending = "\r"      
+    end
+
+    attr_accessor :filename, :frames, :size, :timecode, :started_at, :ended_at
+
+    def initialize
+      @dvgrab_pipe = @capture_output = nil
+      Signal.trap("CHLD") do
+        puts "dvgrab proccses terminated"
+        @dvgrab_pipe = nil
+      end
+    end
+
+    def start!
+      raise "No output file specified" unless configuration[:output_file].present?
+      raise "Already recording" if recording?      
+      nullify_accessors!
+      run_dvgrab
+      capture_output
+      @started_at = Time.now
+    end
+
+    def stop!
+      return unless recording?
+      kill_dvgrab
+      stop_capture_output
+      @ended_at = Time.now
+    end
+
+    def recording?
+      @dvgrab_pipe
+    end
+    
+    def elapsed_time
+      return unless @started_at
+      Time.now.at_beginning_of_day + elapsed_in_seconds
+    end
+    
+    def parse(dvgrab_output)
+      if dvgrab_output =~ /"([.a-zA-Z\/]*)":\s+(.*)\s+MiB\s+(\d+)\s+frames\s+timecode\s+(.*)\s+date/
+        @filename, @size, @frames, @timecode = $1, $2, $3, $4
+      end
+    end
+
+    private
+    
+    def run_dvgrab
+      @dvgrab_pipe = IO.popen(dvgrab_command)      
+    end
+    
+    def kill_dvgrab
+      Process.kill("HUP", @dvgrab_pipe.pid)
+      @dvgrab_pipe.close
+      @dvgrab_pipe = nil      
+    end
+    
+    def capture_output
+      @capture_output = Thread.new do
+        @dvgrab_pipe.each(configuration[:line_ending]) do |line|
+          parse(line)
+        end
+      end      
+    end
+    
+    def stop_capture_output
+      @capture_output.kill
+    end
+    
+    def nullify_accessors!
+      @filename = nil
+      @frames = nil
+      @size = nil
+      @timecode = nil
+      @started_at = nil
+      @ended_at = nil
+    end
+
+    def dvgrab_command
+      "#{configuration[:dvgrab_exeutable]} #{configuration[:dvgrab_args]} #{configuration[:output_file]} 2>&1"
+    end
+    
+    def elapsed_in_seconds
+      return Time.now - @started_at unless @ended_at
+      @ended_at - @started_at
+    end    
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  FireGrabber::Recorder.configure do |config|
+    config.dvgrab_exeutable = '/Users/jon/code/test/fire_grabber/spec/support/dvgrab'
+    config.output_file = '/path/to/file'
+    config.line_ending = "\n"
+  end
+  
+  r = FireGrabber::Recorder.new
+  r.start!
+  sleep 10
+  r.stop!
 end
--- spec/fire_grabber/recorder_spec.rb
+++ spec/fire_grabber/recorder_spec.rb
@@ -3,28 +3,135 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe FireGrabber::Recorder do
 
   before(:each) do
-    FireGrabber::Recorder.configuration[:dvgrab_exeutable] = '/Users/jon/NetBeansProjects/fire_grabber/spec/support/dvgrab'
+    FireGrabber::Recorder.configure do |config|
+      config.output_file = '/path/to/file'
+      config.line_ending = "\n"
+      config.dvgrab_exeutable = File.expand_path("#{File.dirname(__FILE__)}/../support/dvgrab")
+    end
     @r = FireGrabber::Recorder.new
   end
 
   describe "settings" do
+
     it "should respond to configuration" do
       FireGrabber::Recorder.configuration.should be_kind_of(Hash)
     end
-    
+
     it "should have standard setting for dvgrab executable" do
       FireGrabber::Recorder.configuration[:dvgrab_exeutable] = '/path/to/dvgrab'
       FireGrabber::Recorder.configuration[:dvgrab_exeutable].should eql('/path/to/dvgrab')
     end
-    
+
+    it "should have default dvgrab params" do
+      FireGrabber::Recorder.configuration[:dvgrab_args].should eql('-showstatus -noavc -f dv1')
+    end
+
+    it "should have default value for line break of output from dvgrab" do
+      FireGrabber::Recorder.configuration[:line_ending] = "\r"
+    end
+
+    it "should yield configure structur" do
+      FireGrabber::Recorder.configure do |config|
+        config.should be_a_kind_of(OpenStruct)
+      end
+    end
+
+    it "should allow changes in configure block" do
+      FireGrabber::Recorder.configure do |config|
+        config.output_file = "/other/output/file"
+      end
+      FireGrabber::Recorder.configuration[:output_file].should eql("/other/output/file")
+    end
+
   end
 
   describe "record" do
-    
+
     it "should raise error when not output file is set" do
+      FireGrabber::Recorder.configuration[:output_file] = nil
+      lambda{ @r.start! }.should raise_error
+    end
+
+    it "should raise if it's already started" do
       @r.start!
+      lambda{ @r.start! }.should raise_error
+    end
+
+    it "should start recording" do
+      @r.start!
+      @r.should be_recording
+    end
+
+    it "should stop recording" do
+      @r.stop!
+      @r.should_not be_recording
+    end
+
+    it "should nil all attributes on start" do
+      @r.instance_eval { @filename = @frames = @size = @timecode = @started_at = @ended_at = "some value" }
+      @r.start!
+      [:filename, :frames, :size, :timecode, :ended_at].each do |attr|
+        @r.send(attr).should eql(nil)
+      end
+    end
+
+  end
+
+  describe "elapsed time" do
+    
+    before(:each) do
+      @r.stub!(:run_dvgrab)
+      @r.stub!(:kill_dvgrab)
+      @r.stub!(:capture_output)
+      @r.stub!(:stop_capture_output)
+    end
+
+    it "should set started at" do
+      @r.start!
+      @r.started_at.to_s(:db).should eql(Time.now.to_s(:db))
+    end
+
+    it "should set ended at" do
+      @r.stub!(:recording?).and_return(:true)
+      @r.stop!
+      @r.ended_at.to_s(:db).should eql(Time.now.to_s(:db))
+    end
+
+    it "should return elapsed time based on curren time unless ended" do
+      @r.started_at = Time.now - 1.hour
+      @r.elapsed_time.to_s(:time).should eql("01:00")
     end
     
+    it "should return elapsed time based on ended at" do
+      @r.started_at = Time.now - 1.hour
+      @r.ended_at = Time.now + 2.hour
+      @r.elapsed_time.to_s(:time).should eql("03:00")
+    end
+ 
+  end
+
+  describe "parse dvgrab output" do
+
+    it "should get filename" do
+      @r.parse(%Q["/path/to/file.avi":    72.00 MiB  1800 frames timecode 00:01:12.00 date 2009-01-21 21:06:20])
+      @r.filename.should eql("/path/to/file.avi")
+    end
+
+    it "should get size" do
+      @r.parse(%Q["/path/to/file.avi":    72.00 MiB  1800 frames timecode 00:01:12.00 date 2009-01-21 21:06:20])
+      @r.size.should eql("72.00")
+    end
+
+    it "should get frames" do
+      @r.parse(%Q["/path/to/file.avi":    72.00 MiB  1800 frames timecode 00:01:12.00 date 2009-01-21 21:06:20])
+      @r.frames.should eql("1800")
+    end
+
+    it "should get timecode" do
+      @r.parse(%Q["/path/to/file.avi":    72.00 MiB  1800 frames timecode 00:01:12.00 date 2009-01-21 21:06:20])
+      @r.timecode.should eql("00:01:12.00")
+    end
+
   end
 
 end
